fd2c62d130567afe1b097b03212b29dab8c51ff8
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-03-03 14:37:06 +0900

    6.17
    selectors part 2

--- src/components/atoms.tsx
+++ src/components/atoms.tsx
@@ -5,7 +5,10 @@ export interface IToDo {
   id: number;
   category: "TO_DO" | "DOING" | "DONE";
 }
-
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
@@ -14,10 +17,7 @@ export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState);
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ];
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
   },
 });
